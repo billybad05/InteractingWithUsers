58661aa1805b1f43e1d70a86e7fff2ec506e24bb
Billy Dotson <billybad5@gmail.com>
2022-09-14 17:36:18 -0400

    Add project files.

---
+++ InteractingWithUsers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InteractingWithUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter you age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"Hello {name} you are {age}");
+
+            Console.ReadLine();
+        }
+    }
+}
